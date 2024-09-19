eeed5c72e20bb53845ad6920a336d37d4e5aab47
elghanbibadr <bghanbi50@gmail.com>
2024-09-19 09:52:06 +0100

    getting genres  by id

--- project.js
+++ project.js
@@ -1,8 +1,12 @@
 const express=require('express');
-const { Mongoose } = require('mongoose');
 const mongoose=require('mongoose')
 const app=express()
 const router=express.Router();
+
+function generateRandomId() {
+    // Create a random 24-character string
+    return (Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)).substring(0, 24);
+  }
 
 
 // CONNECT TO MONGODB
@@ -24,13 +28,60 @@ const connectDB = async () => {
 
 
 const genreSchema=new mongoose.Schema({
-    id:String,
     name:{type:String,required:true}
 })
-
-
 
 // CLASS MODULE
 const Genres=mongoose.model("Genres",genreSchema)
 
+async function createGenre(){
+    const genre=new Genres({
+        id:generateRandomId(),
+        name:"action"
+    })
+    try{
+        const result=await genre.save();
 
+    }catch(e){
+        console.log("error creating genres",e.message)
+    }
+}
+
+
+
+// createGenre()
+
+// getting all genres from the db
+app.get('/',async (req,res) => {
+  try{
+    const genres=await Genres.find()
+    res.send(genres)
+  }catch(e){
+    return res.send({error:e.message})
+  }
+})
+
+
+// getting genres by id
+
+app.get('/:id',async (req,res) => {
+  const id=req.params.id
+  try{
+    const genre=await Genres.findById(id)
+    if(genre){
+      res.send(genre)
+    }else{
+      res.send("no genres found").status(404)
+    }
+  }catch(e){
+    res.send(e.message)
+  }
+  
+})
+
+
+// listening for changes
+
+app.listen('3000',()=>{
+  console.log("app listening")
+})
