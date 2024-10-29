e478e2048a6719c5eec149fceca7c11705705d4c
elghanbibadr <bghanbi50@gmail.com>
2024-10-29 12:47:15 +0100

    handling the adding of users with the same emails

--- auth/routes/users.js
+++ auth/routes/users.js
@@ -6,16 +6,21 @@ const router=express.Router()
 
 router.post('/',async(req,res)=>{
   
- const {email,password}=req.body
+ const {name,email,password}=req.body
 
  if(!email || !password){
     return res.status(400).send("email and password are required")
  }
 
 //  saving a new user
- try{
+ try{  
+    //  check if a user with this email exists
 
-    let user=new User({email,password})
+    let user=await User.findOne({email})
+    if(user){
+        return res.status(400).send("User already registered !")
+    }
+     user=new User({name,email,password})
      user=await user.save()
     res.send(user)
 
